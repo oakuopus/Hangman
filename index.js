abaae4ebbc0f4d8e48821a2c27319cc1e2ca8cb2
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-04-23 18:19:46 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 $(function() {
-    var words = ["youloser", "words", "trollis", "worldle", "prettier", "canes"]
-    var guessedLetter = [], remainingGuesses = 8
+    var words = [
+        "words", "trollis", "worldle", "prettier", 
+        "canes", "awesome", "random", "zebra", "banana", 
+        "computer", "elephant", "kangaroo", "sunshine", "victory", 
+        "mystery", "adventure", "happiness", "laughter", "kindness", 
+        "wonder", "puzzle", "whisper", "serenity", "harmony", 
+        "blossom", "journey", "courage", "bravery", "strength", 
+        "unicorn", "dragon", "creativity", "inspire", "imagine", 
+        "explore", "discovery", "triumph", "success", "freedom", 
+        "fortune", "spirit", "grace", "beauty", "passion", 
+        "belief", "legend", "magic", "faith", "dream"
+    ];    
+    var guessedLetter = [], remainingGuesses = 5
     var chosenWord = words[Math.floor(Math.random()*words.length)];
 
     for(var i=0; i < chosenWord.length; i++) {
@@ -9,13 +20,30 @@ $(function() {
 
     function updateGuesses(){
         $("#guess-container").empty()
-        $("#guess-container").html("Guessed Letters:<br> " + guessedLetter.join(","));
+        $("#guess-container").html("Guessed Letters:<br> " + guessedLetter.join(", "));
     }
-
+    var image = document.getElementById("image")
+    var wrongguess = 0
     function checkGuess(letter){
         if(chosenWord.indexOf(letter) === -1){
             remainingGuesses--
             $("#remaining-guesses").text(remainingGuesses)
+            if(wrongguess === 0){
+                image.src = "./2.png"
+                wrongguess++
+            }else if(wrongguess === 1){
+                image.src = "./3.png"
+                wrongguess++
+            }else if(wrongguess === 2){
+                image.src = "./4.png"
+                wrongguess++
+            }else if(wrongguess === 3){
+                image.src = "./5.png"
+                wrongguess++
+            }else if(wrongguess === 4){
+                image.src = "./6.png"
+                wrongguess++
+            }
         }else{
             $(".hidden-letter").each(function(index){
                 if(chosenWord[index] === letter){
@@ -39,7 +67,7 @@ $(function() {
 
     function resetGame(){
         guessedLetter = []
-        remainingGuesses = 8
+        remainingGuesses = 5
         $('#remaining-guesses').text(remainingGuesses)
         $('#word-container').empty()
         chosenWord = words[Math.floor(Math.random() * words.length)]
